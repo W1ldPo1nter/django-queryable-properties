3c9b92b4c52f0b77f1e07433d4a6f39b805358bd
Marcus Klöpfel <marcus.kloepfel@gmail.com>
2018-12-11 22:00:19 +0100

    Adapted the special test setup stuff to Django versions < 1.6

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,8 @@
 
 import pytest
 
+from django import VERSION as DJANGO_VERSION
+
 from .models import ApplicationWithClassBasedProperties, ApplicationWithDecoratorBasedProperties
 
 
@@ -32,26 +34,64 @@ except ImportError:
             self.lookup = 'pk'
 
         def as_sql(self, qn, connection):
-            return '({})'.format(self.sql), self.params
+            sql = '({})'.format(self.sql)
+            if DJANGO_VERSION < (1, 6):
+                # Very old Django versions expect the interpolated raw SQL
+                return sql % self.params
+            return sql, self.params
 
         def add_to_query(self, query, alias, col, source, is_summary):
             query.aggregates[alias] = self
 
-    @pytest.fixture(autouse=True)
-    def patch_query_need_having(monkeypatch):
-        """
-        A fixture that monkeypatches a Query method to be able to use the
-        RawSQLAnnotation without a HAVING clause being added in Django
-        versions < 1.8.
-        """
-        original = Query.need_having
+    if DJANGO_VERSION < (1, 6):
+        @pytest.fixture(autouse=True)
+        def patch_query_add_filter(monkeypatch):
+            """
+            A fixture that monkeypatches a Query method to be able to use the
+            RawSQLAnnotation without a HAVING clause being added in Django
+            versions < 1.8.
+            """
+            original = Query.add_filter
 
-        def patched(self, obj):
-            if not isinstance(obj, Node) and isinstance(self.aggregates.get(obj[0], None), RawSQL):
-                return self.aggregates[obj[0]].contains_aggregate
-            return original(self, obj)
+            def patched(self, *args, **kwargs):
+                # add_filter will add any filter clauses that reference an
+                # aggregate to the HAVING clause (instead of WHERE). The work-
+                # around therefore is to check how many HAVING items there were
+                # before the add_filter call and to check all new HAVING items
+                # that were added by the call. If one of them references a
+                # RawSQL instance, it will be moved from HAVING to WHERE.
+                having_count = len(self.having.children)
+                original(self, *args, **kwargs)
+                for entry in self.having.children[having_count:]:
+                    if isinstance(entry[0], RawSQL):
+                        self.having.children.remove(entry)
+                        self.where.children.append(entry)
 
-        monkeypatch.setattr(Query, 'need_having', patched)
+            monkeypatch.setattr(Query, 'add_filter', patched)
+
+    else:
+        @pytest.fixture(autouse=True)
+        def patch_query_need_having(monkeypatch):
+            """
+            A fixture that monkeypatches a Query method to be able to use the
+            RawSQLAnnotation without a HAVING clause being added in Django
+            versions < 1.8.
+            """
+            original = Query.need_having
+
+            def patched(self, obj):
+                # need_having is supposed to return the information if the
+                # a filter needs to be put in a HAVING clause (instead of
+                # WHERE). If may be called with either a Q object (Node) or
+                # a tuple containing the aggregate's alias as the first item.
+                # The latter case can therefore easily be intercepted to check
+                # if the aggregate is a RawSQL instance and then customize the
+                # return value.
+                if not isinstance(obj, Node) and isinstance(self.aggregates.get(obj[0], None), RawSQL):
+                    return self.aggregates[obj[0]].contains_aggregate
+                return original(self, obj)
+
+            monkeypatch.setattr(Query, 'need_having', patched)
 
 
 @pytest.fixture
