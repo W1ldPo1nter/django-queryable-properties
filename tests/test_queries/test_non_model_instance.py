ebd083f51d9a10c64d578aa3700b134cf1a277c6
Marcus Klöpfel <marcus.kloepfel@gmail.com>
2019-06-14 20:55:44 +0200

    Added a custom grouping test for an annotation across relations

--- tests/test_queries/test_non_model_instance.py
+++ tests/test_queries/test_non_model_instance.py
@@ -5,6 +5,7 @@ import pytest
 from django import VERSION as DJANGO_VERSION
 
 from ..models import (ApplicationWithClassBasedProperties, ApplicationWithDecoratorBasedProperties,
+                      CategoryWithClassBasedProperties, CategoryWithDecoratorBasedProperties,
                       VersionWithClassBasedProperties, VersionWithDecoratorBasedProperties)
 
 pytestmark = [pytest.mark.django_db, pytest.mark.usefixtures('versions')]
@@ -20,17 +21,23 @@ class TestAggregateAnnotations(object):
         (ApplicationWithDecoratorBasedProperties, {'version_count__gt': 3}, {4}),
         (ApplicationWithDecoratorBasedProperties, {'version_count': 5}, {}),
     ])
-    def test_values_after_annotate(self, model, filters, expected_version_counts):
+    def test_values_to_limit_fields(self, model, filters, expected_version_counts):
         # Delete one version to create separate version counts
         model.objects.all()[0].versions.all()[0].delete()
         queryset = model.objects.filter(**filters).select_properties('version_count').values('version_count')
         assert all(obj_dict['version_count'] in expected_version_counts for obj_dict in queryset)
 
     @pytest.mark.parametrize('model', [ApplicationWithClassBasedProperties, ApplicationWithDecoratorBasedProperties])
-    def test_values_before_annotate(self, model):
+    def test_values_for_custom_grouping(self, model):
         values = model.objects.values('common_data').select_properties('version_count')
         assert len(values) == 1
         assert values[0]['version_count'] == 8
+
+    @pytest.mark.parametrize('model', [CategoryWithClassBasedProperties, CategoryWithDecoratorBasedProperties])
+    def test_values_for_custom_grouping_across_relation(self, model):
+        queryset = model.objects.values('pk', 'applications__pk').filter(applications__version_count=4)
+        assert len(queryset) == 3
+        assert len(set((obj_dict['pk'], obj_dict['applications__pk']) for obj_dict in queryset)) == 3
 
     @pytest.mark.parametrize('model, filters, expected_version_counts', [
         (ApplicationWithClassBasedProperties, {}, {3, 4}),
