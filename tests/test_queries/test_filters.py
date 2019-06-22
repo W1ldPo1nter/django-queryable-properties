5a3e2a4551bf5ef2929be8b7a96eef1d277830bc
Marcus Klöpfel <marcus.kloepfel@gmail.com>
2019-06-22 13:19:11 +0200

    Restructured some filter tests and added a few more test cases

--- tests/test_queries/test_filters.py
+++ tests/test_queries/test_filters.py
@@ -39,28 +39,26 @@ class TestFilterWithoutAnnotations(object):
     ])
     def test_simple_filter(self, model, filters, expected_count, expected_major_minor):
         queryset = model.objects.filter(filters)
-        assert len(queryset) == expected_count
+        assert queryset.count() == len(queryset) == expected_count
         assert all(obj.major_minor == expected_major_minor for obj in queryset)
 
-    @pytest.mark.parametrize('model', [VersionWithClassBasedProperties, VersionWithDecoratorBasedProperties])
-    def test_filter_without_required_annotation(self, model):
+    @pytest.mark.parametrize('model, property_name, value, expected_count, record_checker', [
         # Filtering the 'version' property is also based on filtering the
         # 'major_minor' property, so this test also tests properties that build
-        # on each other
-        queryset = model.objects.filter(version='1.2.3')
-        assert 'version' not in queryset.query.annotations
-        assert len(queryset) == 2
-        assert all(obj.version == '1.2.3' for obj in queryset)
-
-    @pytest.mark.parametrize('model', [ApplicationWithClassBasedProperties, ApplicationWithDecoratorBasedProperties])
-    def test_filter_without_required_annotation_across_relation(self, model):
-        # Filtering the 'version' property is also based on filtering the
-        # 'major_minor' property, so this test also tests properties that build
-        # on each other
-        queryset = model.objects.filter(versions__version='1.2.3')
-        assert 'versions__version' not in queryset.query.annotations
-        assert len(queryset) == 2
-        assert all(obj.versions.filter(version='1.2.3').exists() for obj in queryset)
+        # on each other.
+        (VersionWithClassBasedProperties, 'version', '1.2.3', 2, lambda obj: obj.version == '1.2.3'),
+        (VersionWithDecoratorBasedProperties, 'version', '1.2.3', 2, lambda obj: obj.version == '1.2.3'),
+        # Filters across relations
+        (ApplicationWithClassBasedProperties, 'versions__version', '1.2.3', 2,
+         lambda obj: obj.versions.filter(version='1.2.3').exists()),
+        (ApplicationWithDecoratorBasedProperties, 'versions__version', '1.2.3', 2,
+         lambda obj: obj.versions.filter(version='1.2.3').exists()),
+    ])
+    def test_filter_without_required_annotation(self, model, property_name, value, expected_count, record_checker):
+        queryset = model.objects.filter(**{property_name: value})
+        assert property_name not in queryset.query.annotations
+        assert queryset.count() == len(queryset) == expected_count
+        assert all(record_checker(obj) for obj in queryset)
 
     @pytest.mark.parametrize('model, filters, expected_remaining_count', [
         (VersionWithClassBasedProperties, models.Q(major_minor='1.3'), 4),
@@ -82,35 +80,50 @@ class TestFilterWithoutAnnotations(object):
 
 class TestFilterWithAggregateAnnotation(object):
 
-    @pytest.mark.parametrize('model, filters, expected_count', [
-        (ApplicationWithClassBasedProperties, models.Q(version_count__gt=3), 2),
-        (ApplicationWithClassBasedProperties, models.Q(version_count=4, name__contains='cool'), 1),
-        (ApplicationWithClassBasedProperties, models.Q(version_count=4) | models.Q(name__contains='cool'), 2),
-        (ApplicationWithClassBasedProperties, models.Q(version_count__gt=3, major_sum__gt=5), 0),
-        (ApplicationWithClassBasedProperties, models.Q(version_count__gt=3) | models.Q(major_sum__gt=5), 2),
-        (ApplicationWithDecoratorBasedProperties, models.Q(version_count__gt=3), 2),
-        (ApplicationWithDecoratorBasedProperties, models.Q(version_count=4, name__contains='cool'), 1),
-        (ApplicationWithDecoratorBasedProperties, models.Q(version_count=4) | models.Q(name__contains='cool'), 2),
-        (ApplicationWithDecoratorBasedProperties, models.Q(version_count__gt=3, major_sum__gt=5), 0),
-        (ApplicationWithDecoratorBasedProperties, models.Q(version_count__gt=3) | models.Q(major_sum__gt=5), 2),
-    ])
-    def test_single_model(self, model, filters, expected_count):
-        queryset = model.objects.filter(filters)
-        assert 'version_count' in queryset.query.annotations
-        assert len(queryset) == expected_count
-        # Check that a property annotation used implicitly by a filter does not
-        # lead to a selection of the property annotation
-        assert all(not model.version_count._has_cached_value(app) for app in queryset)
-
-    @pytest.mark.parametrize('model', [CategoryWithClassBasedProperties, CategoryWithDecoratorBasedProperties])
-    def test_across_relation(self, model):
+    @pytest.mark.parametrize('model, property_name, filters, expected_count', [
+        (ApplicationWithClassBasedProperties, 'version_count', models.Q(version_count__gt=3), 2),
+        (ApplicationWithClassBasedProperties, 'version_count', models.Q(version_count=4, name__contains='cool'), 1),
+        (ApplicationWithClassBasedProperties, 'version_count',
+         models.Q(version_count=4) | models.Q(name__contains='cool'), 2),
+        (ApplicationWithClassBasedProperties, 'version_count', models.Q(version_count__gt=3, major_sum__gt=5), 0),
+        (ApplicationWithClassBasedProperties, 'version_count',
+         models.Q(version_count__gt=3) | models.Q(major_sum__gt=5), 2),
+        (ApplicationWithDecoratorBasedProperties, 'version_count', models.Q(version_count__gt=3), 2),
+        (ApplicationWithDecoratorBasedProperties, 'version_count',
+         models.Q(version_count=4, name__contains='cool'), 1),
+        (ApplicationWithDecoratorBasedProperties, 'version_count',
+         models.Q(version_count=4) | models.Q(name__contains='cool'), 2),
+        (ApplicationWithDecoratorBasedProperties, 'version_count', models.Q(version_count__gt=3, major_sum__gt=5), 0),
+        (ApplicationWithDecoratorBasedProperties, 'version_count',
+         models.Q(version_count__gt=3) | models.Q(major_sum__gt=5), 2),
+        # Filters across relations
         # A query containing an aggregate across a relation is still only
         # grouped by fields of the query's model, so in this case the version
         # count is the total number of application versions per category.
-        queryset = model.objects.filter(applications__version_count=4)
-        assert 'applications__version_count' in queryset.query.annotations
-        assert queryset.count() == 1
-        assert model.objects.filter(applications__version_count=8).count() == 1
+        (CategoryWithClassBasedProperties, 'applications__version_count', models.Q(applications__version_count=4), 1),
+        (CategoryWithDecoratorBasedProperties, 'applications__version_count',
+         models.Q(applications__version_count=4), 1),
+        (CategoryWithClassBasedProperties, 'applications__version_count', models.Q(applications__version_count=8), 1),
+        (CategoryWithDecoratorBasedProperties, 'applications__version_count',
+         models.Q(applications__version_count=8), 1),
+        (CategoryWithClassBasedProperties, 'applications__version_count',
+         models.Q(applications__version_count=4, name__contains='Windows'), 1),
+        (CategoryWithDecoratorBasedProperties, 'applications__version_count',
+         models.Q(applications__version_count=4, name__contains='Windows'), 1),
+        (CategoryWithClassBasedProperties, 'applications__version_count',
+         models.Q(applications__version_count=8, name__contains='Windows'), 0),
+        (CategoryWithDecoratorBasedProperties, 'applications__version_count',
+         models.Q(applications__version_count=8, name__contains='Windows'), 0),
+    ])
+    def test_select_query(self, model, property_name, filters, expected_count):
+        queryset = model.objects.filter(filters)
+        assert property_name in queryset.query.annotations
+        assert queryset.count() == len(queryset) == expected_count
+        if '__' not in property_name:
+            # Check that a property annotation used implicitly by a filter does
+            # not lead to a selection of the property annotation.
+            prop = getattr(model, property_name)
+            assert all(not prop._has_cached_value(app) for app in queryset)
 
     @pytest.mark.parametrize('model, filters, expected_remaining_count', [
         (ApplicationWithClassBasedProperties, models.Q(version_count=4, name__contains='cool'), 1),
@@ -130,36 +143,54 @@ class TestFilterWithAggregateAnnotation(object):
         monkeypatch.setattr(model.version_count, 'get_filter', lambda cls, lookup, value: models.Q(pk__gt=0))
         queryset = model.objects.select_properties('version_count').filter(version_count__gt=5)
         assert '"id" > 0' in six.text_type(queryset.query)
-        assert queryset.count() == 2
+        assert queryset.count() == len(queryset) == 2
 
 
 @pytest.mark.skipif(DJANGO_VERSION < (1, 8), reason="Expression-based annotations didn't exist before Django 1.8")
 class TestFilterWithExpressionAnnotation(object):
 
-    @pytest.mark.parametrize('model', [VersionWithClassBasedProperties, VersionWithDecoratorBasedProperties])
-    def test_single_model(self, model):
-        queryset = model.objects.filter(changes_or_default='(No data)')
-        assert 'changes_or_default' in queryset.query.annotations
-        assert len(queryset) == 6
-        assert all(obj.changes_or_default == '(No data)' for obj in queryset)
-        # Check that a property annotation used implicitly by a filter does not
-        # lead to a selection of the property annotation
-        assert all(not model.changes_or_default._has_cached_value(version) for version in queryset)
-
-    @pytest.mark.parametrize('model', [ApplicationWithClassBasedProperties, ApplicationWithDecoratorBasedProperties])
-    def test_across_relation(self, model):
-        queryset = model.objects.filter(versions__changes_or_default='(No data)')
-        assert 'versions__changes_or_default' in queryset.query.annotations
-        assert len(queryset) == 6
-        assert all(obj.versions.filter(changes_or_default='(No data)').exists() for obj in queryset)
-        assert queryset.distinct().count() == 2
-
-    @pytest.mark.parametrize('model', [CategoryWithClassBasedProperties, CategoryWithDecoratorBasedProperties])
-    def test_dependency_across_relation(self, model):
-        queryset = model.objects.filter(applications__lowered_version_changes='amazing new features')
-        assert 'applications__lowered_version_changes' in queryset.query.annotations
-        assert len(queryset) == 3
-        assert queryset.distinct().count() == 2
+    @pytest.mark.parametrize('model, property_name, filters, expected_count, expected_distinct_count, record_checker', [
+        (VersionWithClassBasedProperties, 'changes_or_default', models.Q(changes_or_default='(No data)'), 6, 6,
+         lambda obj: obj.changes_or_default == '(No data)'),
+        (VersionWithDecoratorBasedProperties, 'changes_or_default', models.Q(changes_or_default='(No data)'), 6, 6,
+         lambda obj: obj.changes_or_default == '(No data)'),
+        (VersionWithClassBasedProperties, 'changes_or_default',
+         models.Q(changes_or_default='(No data)') | models.Q(major=2), 8, 8,
+         lambda obj: obj.changes_or_default == '(No data)' or obj.major == 2),
+        (VersionWithDecoratorBasedProperties, 'changes_or_default',
+         models.Q(changes_or_default='(No data)') | models.Q(major=2), 8, 8,
+         lambda obj: obj.changes_or_default == '(No data)' or obj.major == 2),
+        # Filters across relations
+        (ApplicationWithClassBasedProperties, 'versions__changes_or_default',
+         models.Q(versions__changes_or_default='(No data)'), 6, 2,
+         lambda obj: obj.versions.filter(changes_or_default='(No data)').exists()),
+        (ApplicationWithDecoratorBasedProperties, 'versions__changes_or_default',
+         models.Q(versions__changes_or_default='(No data)'), 6, 2,
+         lambda obj: obj.versions.filter(changes_or_default='(No data)').exists()),
+        (ApplicationWithClassBasedProperties, 'versions__changes_or_default',
+         models.Q(versions__minor=3) & models.Q(versions__changes_or_default='(No data)'), 4, 2,
+         lambda obj: obj.versions.filter(changes_or_default='(No data)').exists()),
+        (ApplicationWithDecoratorBasedProperties, 'versions__changes_or_default',
+         models.Q(versions__minor=3) & models.Q(versions__changes_or_default='(No data)'), 4, 2,
+         lambda obj: obj.versions.filter(changes_or_default='(No data)').exists()),
+        # Filters across relations with dependencies across other relations
+        (CategoryWithClassBasedProperties, 'applications__lowered_version_changes',
+         models.Q(applications__lowered_version_changes='amazing new features'), 3, 2, None),
+        (CategoryWithDecoratorBasedProperties, 'applications__lowered_version_changes',
+         models.Q(applications__lowered_version_changes='amazing new features'), 3, 2, None),
+    ])
+    def test_select_query(self, model, property_name, filters, expected_count, expected_distinct_count, record_checker):
+        queryset = model.objects.filter(filters)
+        assert property_name in queryset.query.annotations
+        assert queryset.distinct().count() == expected_distinct_count
+        assert len(queryset) == expected_count
+        if record_checker:
+            assert all(record_checker(obj) for obj in queryset)
+        if '__' not in property_name:
+            # Check that a property annotation used implicitly by a filter does
+            # not lead to a selection of the property annotation.
+            prop = getattr(model, property_name)
+            assert all(not prop._has_cached_value(version) for version in queryset)
 
     @pytest.mark.parametrize('model, filters, expected_remaining_count', [
         (VersionWithClassBasedProperties, models.Q(changes_or_default='(No data)', major=1), 2),
@@ -179,32 +210,48 @@ class TestFilterWithExpressionAnnotation(object):
         assert '"major" = 2' in pseudo_sql
         assert '"minor" = 0' in pseudo_sql
         assert '"patch" = 0' in pseudo_sql
-        assert queryset.count() == 2
+        assert queryset.count() == len(queryset) == 2
 
 
 @pytest.mark.skipif(DJANGO_VERSION < (1, 11), reason="Explicit subqueries didn't exist before Django 1.11")
 class TestFilterWithSubqueryAnnotation(object):
 
-    @pytest.mark.parametrize('model', [ApplicationWithClassBasedProperties, ApplicationWithDecoratorBasedProperties])
-    def test_single_model(self, model):
-        version_model = model.objects.all()[0].versions.model
-        version_model.objects.filter(version='2.0.0')[0].delete()
-        queryset = model.objects.filter(highest_version='2.0.0')
-        assert 'highest_version' in queryset.query.annotations
-        assert len(queryset) == 1
-        application = queryset[0]
-        assert application.versions.filter(major=2, minor=0, patch=0).exists()
-        # Check that a property annotation used implicitly by a filter does not
-        # lead to a selection of the property annotation
-        assert not model.highest_version._has_cached_value(application)
-
-    @pytest.mark.parametrize('model', [VersionWithClassBasedProperties, VersionWithDecoratorBasedProperties])
-    def test_across_relation(self, model):
-        model.objects.filter(version='2.0.0')[0].delete()
-        queryset = model.objects.filter(application__highest_version='2.0.0')
-        assert 'application__highest_version' in queryset.query.annotations
-        assert len(queryset) == 4
-        assert all(obj.application.highest_version == '2.0.0' for obj in queryset)
+    @pytest.mark.parametrize('model, property_name, filters, expected_count, record_checker', [
+        (ApplicationWithClassBasedProperties, 'highest_version', models.Q(highest_version='2.0.0'), 1,
+         lambda obj: obj.versions.filter(major=2, minor=0, patch=0).exists()),
+        (ApplicationWithDecoratorBasedProperties, 'highest_version', models.Q(highest_version='2.0.0'), 1,
+         lambda obj: obj.versions.filter(major=2, minor=0, patch=0).exists()),
+        (ApplicationWithClassBasedProperties, 'highest_version',
+         models.Q(highest_version='2.0.0') | models.Q(name__startswith='Another'), 1,
+         lambda obj: obj.versions.filter(major=2, minor=0, patch=0).exists()),
+        (ApplicationWithDecoratorBasedProperties, 'highest_version',
+         models.Q(highest_version='2.0.0') | models.Q(name__startswith='Another'), 1,
+         lambda obj: obj.versions.filter(major=2, minor=0, patch=0).exists()),
+        # Filters across relations
+        (VersionWithClassBasedProperties, 'application__highest_version',
+         models.Q(application__highest_version='2.0.0'), 4, lambda obj: obj.application.highest_version == '2.0.0'),
+        (VersionWithDecoratorBasedProperties, 'application__highest_version',
+         models.Q(application__highest_version='2.0.0'), 4, lambda obj: obj.application.highest_version == '2.0.0'),
+        (VersionWithClassBasedProperties, 'application__highest_version',
+         models.Q(application__name__startswith='Another') & models.Q(application__highest_version='2.0.0'), 4,
+         lambda obj: obj.application.highest_version == '2.0.0'),
+        (VersionWithDecoratorBasedProperties, 'application__highest_version',
+         models.Q(application__name__startswith='Another') & models.Q(application__highest_version='2.0.0'), 4,
+         lambda obj: obj.application.highest_version == '2.0.0'),
+    ])
+    def test_select_query(self, model, property_name, filters, expected_count, record_checker):
+        version_model = (VersionWithClassBasedProperties if 'ClassBased' in model.__name__
+                         else VersionWithDecoratorBasedProperties)
+        # Delete one version to create distinct constellations
+        version_model.objects.get(version='2.0.0', application__name__contains='cool').delete()
+        queryset = model.objects.filter(filters)
+        assert property_name in queryset.query.annotations
+        assert queryset.count() == len(queryset) == expected_count
+        if '__' not in property_name:
+            # Check that a property annotation used implicitly by a filter does
+            # not lead to a selection of the property annotation.
+            prop = getattr(model, property_name)
+            assert all(not prop._has_cached_value(version) for version in queryset)
 
     @pytest.mark.parametrize('model, filters, expected_remaining_count', [
         (ApplicationWithClassBasedProperties, models.Q(highest_version='2.0.0', name__contains='cool'), 1),
