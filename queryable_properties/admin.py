ef8f9ca92087a1e00197df86a85f7d9c7f418b2a
Marcus Klöpfel <marcus.kloepfel@gmail.com>
2021-01-05 19:11:14 +0100

    Switched to an approach that extends checks/validators

--- queryable_properties/admin.py
+++ queryable_properties/admin.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from copy import deepcopy
-
+import six
 from django.contrib.admin import ModelAdmin, StackedInline, TabularInline
+from django.contrib.admin.options import BaseModelAdmin
 from django.db.models import F
 
 try:
@@ -12,35 +12,56 @@ except ImportError:
 
 from .compat import chain_queryset, LOOKUP_SEP
 from .managers import QueryablePropertiesQuerySetMixin
-from .utils.internal import resolve_queryable_property
+from .utils.internal import InjectableMixin, resolve_queryable_property
 
 
-class QueryablePropertiesAdminMixin(object):
+# TODO: alternative for Django < 1.9 validators
+class QueryablePropertiesChecksMixin(InjectableMixin):
+
+    # TODO: date_hierarchy
+
+    def _check_ordering_item(self, obj, *args):
+        errors = super(QueryablePropertiesChecksMixin, self)._check_ordering_item(obj, *args)
+        if errors:
+            # The number of arguments differs between old and recent Django
+            # versions.
+            field_name = args[-2]
+
+            if not isinstance(field_name, six.string_types) and Combinable is not None:
+                if isinstance(field_name, Combinable):
+                    field_name = field_name.asc()
+                if isinstance(field_name, OrderBy) and isinstance(field_name.expression, F):
+                    field_name = field_name.expression.name
+                else:
+                    return errors
+            if field_name.startswith('-') or field_name.startswith('+'):
+                field_name = field_name[1:]
+            if resolve_queryable_property(obj.model, field_name.split(LOOKUP_SEP))[0]:
+                return []
+        return errors
+
+
+class QueryablePropertiesAdminMeta(type(BaseModelAdmin)):
+
+    CHECK_ATTRIBUTES = ('checks_class', 'default_validator_class', 'validator_class')
+
+    def __new__(mcs, name, bases, attrs):
+        # Mix a check mixin into any checks/validator class if that hasn't
+        # happened already.
+        for attr in mcs.CHECK_ATTRIBUTES:
+            check_class = attrs.get(attr)
+            for base in bases:
+                if check_class is None:
+                    check_class = getattr(base, attr, None)
+            if check_class and not issubclass(check_class, QueryablePropertiesChecksMixin):
+                class_name = 'QueryableProperties' + check_class.__name__
+                attrs[attr] = QueryablePropertiesChecksMixin.mix_with_class(check_class, class_name)
+        return super(QueryablePropertiesAdminMeta, mcs).__new__(mcs, name, bases, attrs)
+
+
+class QueryablePropertiesAdminMixin(six.with_metaclass(QueryablePropertiesAdminMeta, object)):
 
     list_select_properties = None
-
-    def __init__(self, *args, **kwargs):
-        super(QueryablePropertiesAdminMixin, self).__init__(*args, **kwargs)
-        self._ordering_with_properties = None
-        if self.ordering is not None:
-            # Filter out all queryable property references as they wouldn't
-            # pass Django's checks. Maintain the real ordering in a different
-            # attribute instead.
-            self._ordering_with_properties = deepcopy(self.ordering)
-            self.ordering = [ordering_item for ordering_item in self.ordering
-                             if not self._is_queryable_property_ordering(ordering_item)]
-
-    def _is_queryable_property_ordering(self, ordering_item):
-        if Combinable is not None:
-            if isinstance(ordering_item, Combinable):
-                ordering_item = ordering_item.asc()
-            if isinstance(ordering_item, OrderBy) and isinstance(ordering_item.expression, F):
-                ordering_item = ordering_item.expression.name
-            else:
-                return False
-        if ordering_item.startswith('-') or ordering_item.startswith('+'):
-            ordering_item = ordering_item[1:]
-        return bool(resolve_queryable_property(self.model, ordering_item.split(LOOKUP_SEP))[0])
 
     def get_queryset(self, request):
         queryset = super(QueryablePropertiesAdminMixin, self).get_queryset(request)
@@ -53,9 +74,6 @@ class QueryablePropertiesAdminMixin(object):
         if list_select_properties:
             queryset = queryset.select_properties(*list_select_properties)
         return queryset
-
-    def get_ordering(self, request):
-        return self._ordering_with_properties or super(QueryablePropertiesAdminMixin, self).get_ordering(request)
 
     def get_list_select_properties(self):
         return self.list_select_properties
