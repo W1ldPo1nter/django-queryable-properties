e7c7b0164bfd627621ab0bf7d9ae56bd882387c9
Marcus Klöpfel <marcus.kloepfel@gmail.com>
2021-01-29 22:52:41 +0100

    Added a custom class for errors, date_hierarchy handling and validate property references

--- queryable_properties/admin.py
+++ queryable_properties/admin.py
@@ -3,7 +3,13 @@
 import six
 from django.contrib.admin import ModelAdmin, StackedInline, TabularInline
 from django.contrib.admin.options import BaseModelAdmin
-from django.db.models import F
+from django.core.exceptions import ImproperlyConfigured
+from django.db.models import DateField, F
+
+try:
+    from django.core.checks import Error as BaseError
+except ImportError:
+    BaseError = object
 
 try:
     from django.db.models.expressions import Combinable, OrderBy
@@ -15,30 +21,67 @@ from .managers import QueryablePropertiesQuerySetMixin
 from .utils.internal import InjectableMixin, resolve_queryable_property
 
 
+class Error(BaseError):
+
+    def __init__(self, msg, obj, error_id):
+        error_id = 'queryable_properties.admin.E{:03}'.format(error_id)
+        if BaseError is not object:
+            super(Error, self).__init__(msg, obj=obj, id=error_id)
+        else:
+            self.msg = msg
+            self.obj = obj
+            self.error_id = error_id
+
+    def raise_exception(self):
+        raise ImproperlyConfigured('{}: ({}) {}'.format(six.text_type(self.obj), self.error_id, self.msg))
+
+
 # TODO: alternative for Django < 1.9 validators
 class QueryablePropertiesChecksMixin(InjectableMixin):
 
-    # TODO: date_hierarchy
+    def _check_queryable_property(self, obj, field_name, attribute_name, allow_relation=True):
+        errors = []
+        path = field_name.split(LOOKUP_SEP) if allow_relation else [field_name]
+        property_ref = resolve_queryable_property(obj.model, path)[0]
+        if not property_ref.property.get_annotation:
+            message = '"{}" refers to queryable property "{}", which does not implement annotation creation.'.format(
+                attribute_name, field_name)
+            errors.append(Error(message, obj, error_id=1))
+        return property_ref and property_ref.property, errors
+
+    def _check_date_hierarchy(self, obj):
+        errors = super(QueryablePropertiesChecksMixin, self)._check_date_hierarchy(obj)
+        if not errors:
+            return errors
+
+        prop, property_errors = self._check_queryable_property(obj, obj.date_hierarchy, 'date_hierarchy')
+        if prop and not property_errors:
+            output_field = getattr(prop.get_annotation(obj.model), 'output_field', None)
+            if output_field and not isinstance(output_field, DateField):
+                message = ('"date_hierarchy" refers to queryable property "{}", which does not annotate date values.'
+                           .format(obj.date_hierarchy))
+                property_errors.append(Error(message, obj, error_id=2))
+        return property_errors if prop else errors
 
     def _check_ordering_item(self, obj, *args):
         errors = super(QueryablePropertiesChecksMixin, self)._check_ordering_item(obj, *args)
-        if errors:
-            # The number of arguments differs between old and recent Django
-            # versions.
-            field_name = args[-2]
+        if not errors:
+            return errors
 
-            if not isinstance(field_name, six.string_types) and Combinable is not None:
-                if isinstance(field_name, Combinable):
-                    field_name = field_name.asc()
-                if isinstance(field_name, OrderBy) and isinstance(field_name.expression, F):
-                    field_name = field_name.expression.name
-                else:
-                    return errors
-            if field_name.startswith('-') or field_name.startswith('+'):
-                field_name = field_name[1:]
-            if resolve_queryable_property(obj.model, field_name.split(LOOKUP_SEP))[0]:
-                return []
-        return errors
+        # The number of arguments differs between old and recent Django
+        # versions.
+        field_name = args[-2]
+        if not isinstance(field_name, six.string_types) and Combinable is not None:
+            if isinstance(field_name, Combinable):
+                field_name = field_name.asc()
+            if isinstance(field_name, OrderBy) and isinstance(field_name.expression, F):
+                field_name = field_name.expression.name
+            else:
+                return errors
+        if field_name.startswith('-') or field_name.startswith('+'):
+            field_name = field_name[1:]
+        prop, property_errors = self._check_queryable_property(obj, field_name, 'ordering')
+        return property_errors if prop else errors
 
 
 class QueryablePropertiesAdminMeta(type(BaseModelAdmin)):
