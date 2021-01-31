45b7eaee4f3072535c40bf71d3ff8dda607e2d9c
Marcus Klöpfel <marcus.kloepfel@gmail.com>
2021-01-31 15:18:54 +0100

    Transformed the admin module into a package with its own module for the checks extensions

--- queryable_properties/admin/checks.py
+++ queryable_properties/admin/checks.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 
 import six
-from django.contrib.admin import ModelAdmin, StackedInline, TabularInline
-from django.contrib.admin.options import BaseModelAdmin
 from django.core.exceptions import ImproperlyConfigured
 from django.db.models import DateField, F
 
@@ -16,9 +14,8 @@ try:
 except ImportError:
     Combinable = OrderBy = None
 
-from .compat import chain_queryset, LOOKUP_SEP
-from .managers import QueryablePropertiesQuerySetMixin
-from .utils.internal import InjectableMixin, resolve_queryable_property
+from ..compat import LOOKUP_SEP
+from ..utils.internal import InjectableMixin, resolve_queryable_property
 
 
 class Error(BaseError):
@@ -82,56 +79,3 @@ class QueryablePropertiesChecksMixin(InjectableMixin):
             field_name = field_name[1:]
         prop, property_errors = self._check_queryable_property(obj, field_name, 'ordering')
         return property_errors if prop else errors
-
-
-class QueryablePropertiesAdminMeta(type(BaseModelAdmin)):
-
-    CHECK_ATTRIBUTES = ('checks_class', 'default_validator_class', 'validator_class')
-
-    def __new__(mcs, name, bases, attrs):
-        # Mix a check mixin into any checks/validator class if that hasn't
-        # happened already.
-        for attr in mcs.CHECK_ATTRIBUTES:
-            check_class = attrs.get(attr)
-            for base in bases:
-                if check_class is None:
-                    check_class = getattr(base, attr, None)
-            if check_class and not issubclass(check_class, QueryablePropertiesChecksMixin):
-                class_name = 'QueryableProperties' + check_class.__name__
-                attrs[attr] = QueryablePropertiesChecksMixin.mix_with_class(check_class, class_name)
-        return super(QueryablePropertiesAdminMeta, mcs).__new__(mcs, name, bases, attrs)
-
-
-class QueryablePropertiesAdminMixin(six.with_metaclass(QueryablePropertiesAdminMeta, object)):
-
-    list_select_properties = None
-
-    def get_queryset(self, request):
-        queryset = super(QueryablePropertiesAdminMixin, self).get_queryset(request)
-        # Make sure to use a queryset with queryable properties features.
-        if not isinstance(queryset, QueryablePropertiesQuerySetMixin):
-            queryset = chain_queryset(queryset)
-            QueryablePropertiesQuerySetMixin.inject_into_object(queryset)
-        # Apply list_select_properties.
-        list_select_properties = self.get_list_select_properties()
-        if list_select_properties:
-            queryset = queryset.select_properties(*list_select_properties)
-        return queryset
-
-    def get_list_select_properties(self):
-        return self.list_select_properties
-
-
-class QueryablePropertiesAdmin(QueryablePropertiesAdminMixin, ModelAdmin):
-
-    pass
-
-
-class QueryablePropertiesStackedInline(QueryablePropertiesAdminMixin, StackedInline):
-
-    pass
-
-
-class QueryablePropertiesTabularInline(QueryablePropertiesAdminMixin, TabularInline):
-
-    pass
