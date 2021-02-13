6c1ae44e67d2520de2ea35ad7c5dcf17a123a2e5
Marcus Klöpfel <marcus.kloepfel@gmail.com>
2021-02-13 22:43:36 +0100

    Added an additional admin for tests

--- tests/app_management/admin.py
+++ tests/app_management/admin.py
@@ -3,9 +3,18 @@
 from queryable_properties.admin import QueryablePropertiesAdmin
 
 
+class ApplicationWithClassBasedPropertiesAdmin(QueryablePropertiesAdmin):
+    list_display = ('name', 'highest_version', 'version_count')
+    list_filter = ('common_data', 'support_start_date', 'has_version_with_changelog')
+    search_fields = ('name', 'highest_version')
+    date_hierarchy = 'support_start_date'
+    list_select_properties = ('highest_version', 'version_count')
+    ordering = ('highest_version', 'name')
+
+
 class VersionWithClassBasedPropertiesAdmin(QueryablePropertiesAdmin):
     list_display = ('version', 'application', 'is_supported')
-    list_filter = ('is_supported', 'released_in_2018')
-    search_fields = ('changes', 'version')
+    list_filter = ('is_supported', 'released_in_2018', 'major')
+    search_fields = ('changes',)
     date_hierarchy = 'supported_from'
-    list_select_properties = ('version',)
+    ordering = ('application', 'major', 'minor', 'patch')
