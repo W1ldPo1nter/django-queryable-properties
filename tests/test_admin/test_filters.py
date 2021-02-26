657ee13a3dd6103db7ac6553501b0c268ee7f24f
Marcus Klöpfel <marcus.kloepfel@gmail.com>
2021-02-26 22:59:59 +0100

    Adapted tests to the QueryablePropertyField changes

--- tests/test_admin/test_filters.py
+++ tests/test_admin/test_filters.py
@@ -24,33 +24,31 @@ class TestQueryablePropertyField(object):
         ('version_count', ApplicationWithClassBasedProperties.version_count),
         ('categories__version_count', CategoryWithClassBasedProperties.version_count),
     ])
-    def test_initializer(self, rf, admin_instance, query_path, expected_property):
-        request = rf.get('/')
-        field = QueryablePropertyField(admin_instance, request, query_path)
+    def test_initializer(self, admin_instance, query_path, expected_property):
+        field = QueryablePropertyField(admin_instance, query_path)
         output_field = get_output_field(expected_property.get_annotation(expected_property.model))
         assert type(field.output_field) is type(output_field)  # Old django versions don't implement field comparison
         assert field.model_admin is admin_instance
-        assert field.request is request
         assert field.property is expected_property
         assert field.property_path == query_path
         assert field.null is getattr(output_field, 'null', True)
         assert field.empty_strings_allowed is getattr(output_field, 'null', True)
 
     @pytest.mark.parametrize('query_path', ['name', 'neither_field_nor_property', 'version_count__gt'])
-    def test_initializer_error(self, rf, admin_instance, query_path):
+    def test_initializer_error(self, admin_instance, query_path):
         with pytest.raises(QueryablePropertyError):
-            QueryablePropertyField(admin_instance, rf.get('/'), query_path)
+            QueryablePropertyField(admin_instance, query_path)
 
-    def test_attribute_passthrough(self, rf, admin_instance):
-        field = QueryablePropertyField(admin_instance, rf.get('/'), 'version_count')
+    def test_attribute_passthrough(self, admin_instance):
+        field = QueryablePropertyField(admin_instance, 'version_count')
         assert field.model is ApplicationWithClassBasedProperties
         assert field.name == 'version_count'
         assert field.verbose_name == 'Version count'
 
     @pytest.mark.skipif(DJANGO_VERSION < (1, 8), reason="Expression-based annotations didn't exist before Django 1.8")
-    def test_flatchoices_mapping_property(self, rf):
+    def test_flatchoices_mapping_property(self):
         admin = VersionAdmin(VersionWithClassBasedProperties, site)
-        field = QueryablePropertyField(admin, rf.get('/'), 'release_type_verbose_name')
+        field = QueryablePropertyField(admin, 'release_type_verbose_name')
         assert tuple(field.flatchoices) == (
             ('Alpha', 'Alpha'),
             ('Beta', 'Beta'),
@@ -58,30 +56,31 @@ class TestQueryablePropertyField(object):
             (None, field.empty_value_display),
         )
 
-    def test_flatchoices_boolean_property(self, admin_instance, rf):
-        field = QueryablePropertyField(admin_instance, rf.get('/'), 'has_version_with_changelog')
+    @pytest.mark.skipif(DJANGO_VERSION < (1, 8), reason="Expression-based annotations didn't exist before Django 1.8")
+    def test_flatchoices_boolean_property(self, admin_instance):
+        field = QueryablePropertyField(admin_instance, 'has_version_with_changelog')
         assert tuple(field.flatchoices) == ()
 
     @pytest.mark.django_db
-    def test_flatchoices_other_properties(self, versions, admin_instance, rf):
+    @pytest.mark.parametrize('query_path, expected_choices', [
+        ('version_count', ((3, 3), (4, 4))),
+        ('categories__version_count', ((4, 4), (7, 7))),
+    ])
+    def test_flatchoices_other_properties(self, versions, admin_instance, query_path, expected_choices):
         versions[0].delete()
-        field = QueryablePropertyField(admin_instance, rf.get('/'), 'version_count')
-        assert tuple(field.flatchoices) == (
-            (3, 3),
-            (4, 4),
-        )
+        field = QueryablePropertyField(admin_instance, query_path)
+        assert tuple(field.flatchoices) == expected_choices
 
     @pytest.mark.parametrize('filter_class, expected_filter_class', [
         (None, DateFieldListFilter),
         (ChoicesFieldListFilter, ChoicesFieldListFilter),
     ])
     def test_get_filter_creator(self, rf, admin_instance, filter_class, expected_filter_class):
-        request = rf.get('/')
-        field = QueryablePropertyField(admin_instance, request, 'support_start_date')
+        field = QueryablePropertyField(admin_instance, 'support_start_date')
         field.output_field = DateField(null=True)  # To set an output field for Django versions that don't support it
         creator = field.get_filter_creator(filter_class)
         assert callable(creator)
-        list_filter = creator(request, {}, admin_instance.model, admin_instance)
+        list_filter = creator(rf.get('/'), {}, admin_instance.model, admin_instance)
         assert isinstance(list_filter, expected_filter_class)
         assert list_filter.field is field
         assert list_filter.field_path == field.property_path
@@ -96,6 +95,6 @@ class TestQueryablePropertyListFilter(object):
         (ApplicationWithClassBasedProperties.support_start_date, VersionAdmin, DateFieldListFilter),
         (VersionWithClassBasedProperties.version, VersionAdmin, ChoicesFieldListFilter),
     ])
-    def test_get_class(self, rf, prop, admin_class, expected_filter_class):
-        field = QueryablePropertyField(admin_class(prop.model, site), rf.get('/'), prop.name)
+    def test_get_class(self, prop, admin_class, expected_filter_class):
+        field = QueryablePropertyField(admin_class(prop.model, site), prop.name)
         assert QueryablePropertyListFilter.get_class(field) is expected_filter_class
