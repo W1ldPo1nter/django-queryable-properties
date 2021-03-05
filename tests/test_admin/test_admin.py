77c55afbe8249d7ac9cb8dc1891d143a08cb945c
Marcus Klöpfel <marcus.kloepfel@gmail.com>
2021-03-05 22:29:31 +0100

    Adapted list filter tests and added tests regarding searching by queryable properties

--- tests/test_admin/test_admin.py
+++ tests/test_admin/test_admin.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
 import pytest
+from django import VERSION as DJANGO_VERSION
 from django.contrib.admin import site
 from django.contrib.admin.filters import ChoicesFieldListFilter, FieldListFilter
 from django.db.models.query import QuerySet
 from mock import patch
 
-from queryable_properties.compat import nullcontext
+from queryable_properties.compat import ADMIN_QUERYSET_METHOD_NAME, nullcontext
 from queryable_properties.managers import QueryablePropertiesQuerySetMixin
 from ..app_management.admin import ApplicationAdmin, VersionAdmin, VersionInline
 from ..app_management.models import ApplicationWithClassBasedProperties, VersionWithClassBasedProperties
@@ -36,7 +37,8 @@ class TestQueryablePropertiesAdminMixin(object):
         admin = admin_class(model, site)
         qs_patch = nullcontext()
         if apply_patch:
-            qs_patch = patch('django.contrib.admin.options.ModelAdmin.get_queryset', return_value=QuerySet(model))
+            qs_patch = patch('django.contrib.admin.options.ModelAdmin.{}'.format(ADMIN_QUERYSET_METHOD_NAME),
+                             return_value=QuerySet(model))
 
         with qs_patch:
             queryset = admin.get_queryset(rf.get('/'))
@@ -55,7 +57,9 @@ class TestQueryablePropertiesAdminMixin(object):
     def test_get_list_filter(self, monkeypatch, rf, list_filter_item, property_name):
         monkeypatch.setattr(ApplicationAdmin, 'list_filter', ('common_data', list_filter_item))
         admin = ApplicationAdmin(ApplicationWithClassBasedProperties, site)
-        list_filter = admin.get_list_filter(rf.get('/'))
+        list_filter = admin.list_filter
+        if DJANGO_VERSION >= (1, 5):
+            list_filter = admin.get_list_filter(rf.get('/'))
         assert list_filter[0] == 'common_data'
         assert (list_filter[1] == list_filter_item) is (not property_name)
         if property_name:
@@ -64,3 +68,25 @@ class TestQueryablePropertiesAdminMixin(object):
             filter_instance = replacement(rf.get('/'), {}, admin.model, admin)
             assert isinstance(filter_instance, FieldListFilter)
             assert filter_instance.field.name == property_name
+
+    @pytest.mark.skipif(DJANGO_VERSION < (2, 1), reason='Arbitrary search fields were not allowed before Django 2.1')
+    @pytest.mark.django_db
+    @pytest.mark.parametrize('search_term, expected_count', [
+        ('app', 2),
+        ('cool', 1),
+        ('another', 1),
+        ('not-found', 0),
+        ('2.0.0', 1),
+        ('1.3.1', 1),
+        ('1.3', 1),
+        ('1.3.0', 0),
+        ('1.2.3', 0),
+        ('3.4.5', 0),
+    ])
+    @pytest.mark.usefixtures('versions')
+    def test_get_search_results(self, rf, applications, search_term, expected_count):
+        applications[0].versions.filter(version='2.0.0').delete()
+        request = rf.get('/')
+        admin = ApplicationAdmin(ApplicationWithClassBasedProperties, site)
+        queryset = admin.get_search_results(request, admin.get_queryset(request), search_term)[0]
+        assert queryset.count() == expected_count
