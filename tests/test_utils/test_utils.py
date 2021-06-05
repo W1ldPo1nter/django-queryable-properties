7baf5541a25250c2132696afdebf0788becf0081
Marcus Klöpfel <marcus.kloepfel@gmail.com>
2021-06-05 19:21:33 +0200

    Added tests for the prefetch_queryable_properties function

--- tests/test_utils/test_utils.py
+++ tests/test_utils/test_utils.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 
+from itertools import groupby
+
 import pytest
 
 from queryable_properties.exceptions import QueryablePropertyDoesNotExist
 from queryable_properties.properties import QueryableProperty
-from queryable_properties.utils import get_queryable_property
+from queryable_properties.utils import get_queryable_property, prefetch_queryable_properties
 
-from ..app_management.models import VersionWithClassBasedProperties, VersionWithDecoratorBasedProperties
+from ..app_management.models import (ApplicationWithClassBasedProperties, CategoryWithClassBasedProperties,
+                                     VersionWithClassBasedProperties, VersionWithDecoratorBasedProperties)
 
 
 class TestGetQueryableProperty(object):
@@ -30,3 +33,85 @@ class TestGetQueryableProperty(object):
     def test_exception(self, model, property_name):
         with pytest.raises(QueryablePropertyDoesNotExist):
             get_queryable_property(model, property_name)
+
+
+@pytest.mark.django_db
+class TestPrefetchQueryableProperties(object):
+
+    def assert_not_cached(self, prop, *model_instances):
+        assert all(not prop._has_cached_value(instance) for instance in model_instances)
+
+    def assert_cached(self, prop, *model_instances):
+        assert all(prop._has_cached_value(instance) for instance in model_instances)
+        assert all(prop._get_cached_value(instance) == prop.get_value(instance) for instance in model_instances)
+
+    def test_no_instances(self):
+        instances = []
+        prefetch_queryable_properties(instances, 'whatever')
+        assert instances == []
+
+    @pytest.mark.parametrize('props', [
+        (),
+        (ApplicationWithClassBasedProperties.version_count,),
+        (ApplicationWithClassBasedProperties.version_count, ApplicationWithClassBasedProperties.major_sum),
+    ])
+    @pytest.mark.usefixtures('versions')
+    def test_local_property(self, applications, props):
+        applications = applications[:2]
+        for prop in props:
+            self.assert_not_cached(prop, *applications)
+        prefetch_queryable_properties(applications, *(prop.name for prop in props))
+        for prop in props:
+            self.assert_cached(prop, *applications)
+
+    @pytest.mark.usefixtures('versions')
+    def test_2o_relation_property(self):
+        prop = ApplicationWithClassBasedProperties.version_count
+        versions = list(VersionWithClassBasedProperties.objects.select_related('application'))
+        self.assert_not_cached(prop, *(version.application for version in versions))
+        prefetch_queryable_properties(versions, 'application__version_count')
+        self.assert_cached(prop, *(version.application for version in versions))
+
+    @pytest.mark.usefixtures('versions')
+    def test_2m_relation_property(self):
+        prop = ApplicationWithClassBasedProperties.version_count
+        categories = list(CategoryWithClassBasedProperties.objects.prefetch_related('applications'))
+        for category in categories:
+            self.assert_not_cached(prop, *category.applications.all())
+        prefetch_queryable_properties(categories, 'applications__version_count')
+        for category in categories:
+            self.assert_cached(prop, *category.applications.all())
+
+    @pytest.mark.usefixtures('versions')
+    def test_different_types(self, categories, applications):
+        model_instances = categories[:2] + applications
+        grouped_instances = {cls: list(instances) for cls, instances in
+                             groupby(model_instances, key=lambda instance: instance.__class__)}
+        for cls, instances in grouped_instances.items():
+            self.assert_not_cached(cls.version_count, *instances)
+        prefetch_queryable_properties(model_instances, 'version_count')
+        for cls, instances in grouped_instances.items():
+            self.assert_cached(cls.version_count, *instances)
+
+    @pytest.mark.usefixtures('versions')
+    def test_refresh_cache(self, applications):
+        """
+        Test that prefetch_queryable_properties can be used to update already
+        cached values on instances.
+        """
+        prop = ApplicationWithClassBasedProperties.version_count
+        application = ApplicationWithClassBasedProperties.objects.select_properties('version_count').first()
+        self.assert_cached(prop, application)
+        application.versions.all().delete()
+        assert application.version_count > 0  # The cached value is still present
+        prefetch_queryable_properties([application], 'version_count')
+        self.assert_cached(prop, application)
+
+    @pytest.mark.parametrize('property_name, expected_exception', [
+        ('non_existent', QueryablePropertyDoesNotExist),
+        ('application__non_existent', QueryablePropertyDoesNotExist),
+        ('non_existent__non_existent', AttributeError),
+    ])
+    def test_invalid_propery_path(self, versions, property_name, expected_exception):
+        with pytest.raises(expected_exception):
+            prefetch_queryable_properties(versions, property_name)
