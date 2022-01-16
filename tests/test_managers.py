874435987d5e053d66d67b815a7aea93ac4158de
Marcus Klöpfel <marcus.kloepfel@gmail.com>
2022-01-16 19:43:35 +0100

    Added tests for the AggregatePropertyChecker

---
+++ tests/test_managers.py
@@ -0,0 +1,26 @@
+# encoding: utf-8
+
+import pytest
+
+from six.moves import cPickle
+
+from .app_management.models import ApplicationWithClassBasedProperties, ApplicationWithDecoratorBasedProperties
+
+
+class QueryablePropertiesQuerySetMixin(object):
+
+    @pytest.mark.django_db
+    @pytest.mark.usefixtures('versions')
+    @pytest.mark.parametrize('model', [ApplicationWithClassBasedProperties, ApplicationWithDecoratorBasedProperties])
+    def test_pickle_unpickle(self, model):
+        queryset1 = model.objects.filter(version_count=4).order_by('name').select_properties('version_count')
+        expected_applications = list(queryset1)
+        serialized_query = cPickle.dumps(queryset1.query)
+        queryset2 = model.objects.all()
+        queryset2.query = cPickle.loads(serialized_query)
+        serialized_queryset = cPickle.dumps(queryset1)
+        queryset3 = cPickle.loads(serialized_queryset)
+        for queryset in (queryset1, queryset2, queryset3):
+            versions = list(queryset)
+            assert versions == expected_applications
+            assert all(model.version_count.has_cached_value(obj) for obj in queryset)
---
+++ tests/test_query.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+from queryable_properties.query import AggregatePropertyChecker
+from .app_management.models import (ApplicationWithClassBasedProperties, CategoryWithClassBasedProperties,
+                                    VersionWithClassBasedProperties)
+
+
+class TestAggregatePropertyChecker(object):
+
+    def test_initializer(self):
+        checker = AggregatePropertyChecker()
+        assert checker.func == checker.is_aggregate_property
+
+    @pytest.mark.parametrize('model, path, value, expected_result', [
+        # Not a queryable property
+        (ApplicationWithClassBasedProperties, 'non_existent', None, False),
+        # Queryable property with required aggregate annotation
+        (ApplicationWithClassBasedProperties, 'version_count', 1337, True),
+        # Queryable property without required annotation
+        (VersionWithClassBasedProperties, 'version', '1.2.3', False),
+        # Self-references don't lead to infinite loops
+        (CategoryWithClassBasedProperties, 'circular', None, False),
+    ])
+    def test_is_aggregate_property(self, model, path, value, expected_result):
+        assert AggregatePropertyChecker().is_aggregate_property((path, value), model) is expected_result
