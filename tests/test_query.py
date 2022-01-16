3f115070ecf1aa3174dea30804b1352d3e364db5
Marcus Klöpfel <marcus.kloepfel@gmail.com>
2022-01-16 20:05:35 +0100

    Fixed aggregate check for very old Django versions

--- tests/test_query.py
+++ tests/test_query.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import pytest
+from django import VERSION as DJANGO_VERSION
 
 from queryable_properties.query import AggregatePropertyChecker
 from .app_management.models import (ApplicationWithClassBasedProperties, CategoryWithClassBasedProperties,
@@ -21,7 +22,7 @@ class TestAggregatePropertyChecker(object):
         # Queryable property without required annotation
         (VersionWithClassBasedProperties, 'version', '1.2.3', False),
         # Self-references don't lead to infinite loops
-        (CategoryWithClassBasedProperties, 'circular', None, False),
+        (CategoryWithClassBasedProperties, 'circular', None, DJANGO_VERSION < (1, 8)),
     ])
     def test_is_aggregate_property(self, model, path, value, expected_result):
         assert AggregatePropertyChecker().is_aggregate_property((path, value), model) is expected_result
