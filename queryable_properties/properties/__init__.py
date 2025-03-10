b0e3f44eb87281798a10637ab8b115d39cda3d38
Marcus Klöpfel <marcus.kloepfel@rheinwerk-verlag.de>
2025-03-10 16:08:29 +0100

    Added the InheritanceModelProperty

--- queryable_properties/properties/__init__.py
+++ queryable_properties/properties/__init__.py
@@ -4,6 +4,7 @@ from ..utils import MISSING_OBJECT
 from .annotation import AggregateProperty, AnnotationProperty, RelatedExistenceCheckProperty
 from .base import QueryableProperty, queryable_property
 from .cache_behavior import CACHE_RETURN_VALUE, CACHE_VALUE, CLEAR_CACHE, DO_NOTHING
+from .inheritance import InheritanceModelProperty
 from .mixins import (
     REMAINING_LOOKUPS, AnnotationGetterMixin, AnnotationMixin, LookupFilterMixin, SetterMixin, UpdateMixin,
     boolean_filter, lookup_filter,
@@ -16,6 +17,7 @@ __all__ = (
     'AggregateProperty', 'AnnotationProperty', 'RelatedExistenceCheckProperty',
     'QueryableProperty', 'queryable_property',
     'CACHE_RETURN_VALUE', 'CACHE_VALUE', 'CLEAR_CACHE', 'DO_NOTHING',
+    'InheritanceModelProperty',
     'AnnotationGetterMixin', 'AnnotationMixin', 'boolean_filter', 'LookupFilterMixin', 'lookup_filter',
     'REMAINING_LOOKUPS', 'SetterMixin', 'UpdateMixin',
     'MappingProperty', 'RangeCheckProperty', 'ValueCheckProperty',
