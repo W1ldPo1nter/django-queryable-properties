5f7b92756fcf956179a420623e665e3d2a50cc41
Marcus Klöpfel <marcus.kloepfel@rheinwerk-verlag.de>
2026-05-22 13:28:48 +0200

    Moved the new setting to an object in __init__.py

--- queryable_properties/settings.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from django.conf import settings
-
-#: Determines whether annotatable queryable properties apply Django's fetch modes.
-APPLY_FETCH_MODE = getattr(settings, 'QUERYABLE_PROPERTIES_APPLY_FETCH_MODE', False)
